3f6a452df72c750a9bbfdca92b5ebc7ae91c6cc8
Mikael Vesavuori <mikaelvesavuori@gmail.com>
2022-02-08 21:01:25 +0100

    Update

--- scripts.sh
+++
@@ -1,5 +0,0 @@
-docker build -t mikaelvesavuori/dorametrix-pipe:0.0.1 .
-docker run -ti --rm mikaelvesavuori/dorametrix-pipe:0.0.1 abc
-docker push
-
-docker buildx build --platform linux/amd64 --push -t mikaelvesavuori/dorametrix-pipe:0.0.1 .
